c95a0e4db052b6f83ef565329f5f56148d953403
Karthic Rao <kartronics85@gmail.com>
2013-05-30 16:04:03 +0530

    File operation handling for write , read , close and open is written and device file creation is automated by sending events to udev using class_create and device_create API's

--- In-memory-scull-driver/fops.c
+++ In-memory-scull-driver/fops.c
@@ -1,21 +1,85 @@
-#include<linux/cdev.h>
-#include<linux/types.h>
-#include<kdev_t.h>
-#include<linux/fs.h>
-struct rao_dev { 
-	struct rao_qset *data ; /*pointer to the starting address of the data*/
-	int quantum ; 
-	int qset; 
- 	unsigned long size ; 
-	struct cdev cdev ; /*char device structure*/
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+static dev_t first; // Global variable for the first device number 
+static struct cdev c_dev; // Global variable for the character device structure
+static struct class *cl; // Global variable for the device class
+static int simulate_open(struct inode *i, struct file *f)
+{
+  printk(KERN_INFO "Driver: open()\n");
+  return 0;
+}
+  static int simulate_close(struct inode *i, struct file *f)
+{
+  printk(KERN_INFO "Driver: close()\n");
+  return 0;
+}
+  static ssize_t simulate_read(struct file *f, char __user *buf, size_t
+  len, loff_t *off)
+{
+  printk(KERN_INFO "Driver: read()\n");
+  return 0;
+}
+  static ssize_t simulate_write(struct file *f, const char __user *buf,
+  size_t len, loff_t *off)
+{
+  printk(KERN_INFO "Driver: write()\n");
+  return len;
+}
+  static struct file_operations simulate_fops =
+{
+  .owner = THIS_MODULE,
+  .open = simulate_open,
+  .release = simulate_close,
+  .read = simulate_read,
+  .write = simulate_write
 };
-struct file_operations rao_fops {
-	.owner = THIS_MODULE,
-	.read  = rao_read,
-	.write = rao_write,
-	.open  = rao_open,
-	.release = rao_release,
-};
-
-static void rao_setup_cdev(struct rao_dev *dev) { 
-	
+ 
+static int __init ofcd_init(void) /* Constructor */
+{
+  printk(KERN_INFO "\nModule Inserted");
+  if (alloc_chrdev_region(&first, 0, 1, "") < 0)
+  {
+    return -1;
+  }
+    if ((cl = class_create(THIS_MODULE, "simulate_driver")) == NULL)
+  {
+    unregister_chrdev_region(first, 1);
+    return -1;
+  }
+    if (device_create(cl, NULL, first, NULL, "RAO_SIM") == NULL)
+  {
+    class_destroy(cl);
+    unregister_chrdev_region(first, 1);
+    return -1;
+  }
+    cdev_init(&c_dev, &simulate_fops);
+    if (cdev_add(&c_dev, first, 1) == -1)
+  {
+    device_destroy(cl, first);
+    class_destroy(cl);
+    unregister_chrdev_region(first, 1);
+    return -1;
+  }
+  return 0;
+}
+ 
+static void __exit ofcd_exit(void) /* Destructor */
+{
+  cdev_del(&c_dev);
+  device_destroy(cl, first);
+  class_destroy(cl);
+  unregister_chrdev_region(first, 1);
+  printk(KERN_INFO "\nModule removed");
+}
+ 
+module_init(ofcd_init);
+module_exit(ofcd_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Karthic Rao");
