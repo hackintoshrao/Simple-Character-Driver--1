b69ec088c05472dc41ceab5d59bc0614aa40d6e4
Karthic Rao <kartronics85@gmail.com>
2013-05-30 08:29:52 +0530

    writing the scull driver

--- In-memory-scull-driver/fops.c
+++ In-memory-scull-driver/fops.c
@@ -1,7 +1,21 @@
-struct kar_dev { 
-	struct kar_qset *data ; /*pointer to the starting address of the data*/
+#include<linux/cdev.h>
+#include<linux/types.h>
+#include<kdev_t.h>
+#include<linux/fs.h>
+struct rao_dev { 
+	struct rao_qset *data ; /*pointer to the starting address of the data*/
 	int quantum ; 
 	int qset; 
  	unsigned long size ; 
 	struct cdev cdev ; /*char device structure*/
 };
+struct file_operations rao_fops {
+	.owner = THIS_MODULE,
+	.read  = rao_read,
+	.write = rao_write,
+	.open  = rao_open,
+	.release = rao_release,
+};
+
+static void rao_setup_cdev(struct rao_dev *dev) { 
+	
